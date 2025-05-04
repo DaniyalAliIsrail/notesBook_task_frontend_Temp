fb95347e6a0d1adbde89c5b68ce8b2fb636d9b4a
daniyal ali <daniyalali12568@gmail.com>
2025-05-05 04:54:50 +0500

    add_backend_Deploye_link_viteconfig

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target: 'http://localhost:4000',
+        target: 'https://notes-backend-eta-three.vercel.app/',
         secure: false,
       }
     },
